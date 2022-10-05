4869bbf7decf8651a48bcac6afdd93723f0d6f2f
veronezefelipe <96065689+veronezefelipe@users.noreply.github.com>
2022-10-04 23:07:07 -0300

    Add files via upload

---
+++ ConversaoDeUnidadesDeVolume.java
@@ -0,0 +1,27 @@
+package ex03;
+
+import java.text.DecimalFormat;
+
+public class ConversaoDeUnidadesDeVolume {
+	
+	private int quantidade = 0;
+	
+	static DecimalFormat formatador = new DecimalFormat("0.00");
+	
+
+	public static void Litro_para_centimetro_cubicos(int quantidade) {
+		System.out.println("a sua conversão de litros para centímetros cúbicos é: "+formatador.format(quantidade * 1000)+"cm3");
+	}
+	public static void Metro_cubico_para_litros(int quantidade) {
+		System.out.println("a sua conversão de metros cúbicos para litros é: "+formatador.format(quantidade * 1000)+" litros");
+	}
+	public static void  Metro_cubico_para_pes_cubicos(int quantidade) {
+		System.out.println("a sua conversão de metros cúbicos para pés cúbicos é: "+ formatador.format(quantidade * 35.32)+" pés cúbicos");
+	}
+	public static void Galao_americano_para_polegadas_cubicas(int quantidade) {
+		System.out.println("a sua conversão de galão americano para polegadas cúbicas é: "+ formatador.format(quantidade * 231)+" polegadas cúbicas");
+	}
+	public static void Galao_americano_para_litros(int quantidade) {
+		System.out.println("a sua conversão de galão americano para litros é: "+ formatador.format(quantidade * 3785)+" litros");
+	}
+}
---
+++ Principal.java
@@ -0,0 +1,13 @@
+package ex03;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		ConversaoDeUnidadesDeVolume c1 = new ConversaoDeUnidadesDeVolume();
+		c1.Litro_para_centimetro_cubicos(10);
+		c1.Metro_cubico_para_pes_cubicos(10);
+		c1.Metro_cubico_para_litros(10);
+		c1.Galao_americano_para_polegadas_cubicas(10);
+		c1.Galao_americano_para_litros(10);
+	}
+}
