f38f27d36c451e24f88a7f95ca2f04a091d955b2
veronezefelipe <96065689+veronezefelipe@users.noreply.github.com>
2022-10-04 23:02:24 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -3,28 +3,28 @@ package ex01;
 public class Principal {
 
     public static void main(String[] args) {
-        Tempo t1 = new Tempo(2, 54, 6);
+        Tempo temp1 = new Tempo(2, 54, 6);
 
-        System.out.println("Tempo 1 :" +t1.toString());
-        System.out.println("Tempo 1 em segundos: "+t1.tempoEmSegundos());
+        System.out.println("Tempo 1 :" +temp1.toString());
+        System.out.println("Tempo 1 em segundos: "+temp1.tempoEmSegundos());
 
-        Tempo t2 = new Tempo(5, 40, 47);
-        System.out.println("Tempo 2: "+ t2.toString());
-        System.out.println("Tempo 2 em segundos: "+ t2.tempoEmSegundos());
-        System.out.println("Diferença de tempos em segundos: " + t1.difTempo(t2));
+        Tempo temp2 = new Tempo(5, 40, 47);
+        System.out.println("Tempo 2: "+ temp2.toString());
+        System.out.println("Tempo 2 em segundos: "+ temp2.tempoEmSegundos());
+        System.out.println("Diferença de tempos em segundos: " + temp1.difTempo(temp2));
 
-        if(t2.setHora(7)){
+        if(temp2.setHora(7)){
             System.out.println("Nova hora = 7");
         }else System.out.println("ERRO, hora inválida");
 
-        if(t2.setMinuto(74)){
+        if(temp2.setMinuto(74)){
             System.out.println("Novo minuto = 74");
         }else System.out.println("ERRO, minuto inválido");
 
-        if (t2.setSegundo(60)){
+        if (temp2.setSegundo(60)){
             System.out.println("Novo segundo = 60");
         }else System.out.println("ERRO, segundo inválido");
 
-        System.out.println("Novo tempo 2: " + t2.toString());
+        System.out.println("Novo tempo 2: " + temp2.toString());
     }
 }
